993d3dc43d6c4bd0a2191613615075e6b0c5dab7
Tristan <tristan@Tristans-MacBook-Pro.local>
2017-06-24 23:01:31 +0200

    open new file

--- src/switch.ts
+++ src/switch.ts
@@ -3,6 +3,19 @@ import * as vscode from 'vscode';
 import * as _ from "lodash";
 
 export function switchTo(extension:string) {
+
+    // Get Current File
     const filename = _.get(vscode, "window.activeTextEditor.document.fileName")
-    vscode.window.showInformationMessage("openfile " + filename)
+    if(!filename) return;
+
+    // Create New Filename
+    const splits = filename.split('.');
+    const currentExtension = splits.pop()
+    const commonFilename = splits.join('.')
+    const newFilename = commonFilename + '.' + extension
+
+    // Open New Document
+    vscode.workspace.openTextDocument(newFilename)
+        .then(document => vscode.window.showTextDocument(document))
+        .then(console.log)
 }
